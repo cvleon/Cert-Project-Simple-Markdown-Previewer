1d452915002fc24a66cc40470230f00ce7b10531
Christian Vasquez Leon <vasquezleon.christian@gmail.com>
2023-11-06 21:11:13 -0600

    Implement basic markdown previewer

--- simple-markdown-previewer/src/App.js
+++ simple-markdown-previewer/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { marked } from "marked";
+import "./App.css";
 
 function App() {
+  let [text, setText] = useState(`
+  # H1
+  ## H2
+  **bold text**
+  [title](https://www.example.com)
+  ![alt text](image.jpg)
+
+  1. First item
+  2. Second item
+  3. Third item
+
+  - First item
+  - Second item
+  - Third item
+
+  \`code\`
+
+  \`\`\` 
+  {
+    "firstName": "Christian",
+    "lastName": "Vasquez Leon",
+    "age": 24
+  }
+  \`\`\`
+
+  > blockquote
+  
+  `);
+
+  marked.setOptions({
+    breaks: true
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        id="editor"
+        onChange={(event) => setText(event.target.value)}
+        value={text}
+      ></textarea>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: marked(text),
+        }}
+      ></div>
     </div>
   );
 }
